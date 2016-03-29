cd5e9e8b8c9133e4930862f8829984d52eb89176
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2016-03-29 19:35:13 -0400

    Create Custom BookAdmin

--- books/admin.py
+++ books/admin.py
@@ -3,6 +3,29 @@ from .models import Book, Author
 
 # Register your models here.
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Book Details",{"fields":["title","author"]}),
+        ("Book Review",{"fields":["is_favourite", "review", "date_review"]})
+    ]
+
+    readonly_fields = ("date_review",)
+
+    def book_authors(self, obj):
+        return obj.list_authors()
+
+    book_authors.short_description = "Author(s)"
+
+    list_display = ("title","book_authors","date_review","is_favourite",)
+    list_editable = ("is_favourite",)
+    list_display_links = ("title","date_review",)
+    list_filter = ("is_favourite",)
+    search_fields = ("title","author__name")
+
+
+
+
+#admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
 
--- books/models.py
+++ books/models.py
@@ -17,7 +17,7 @@ class Book(models.Model):
     def list_authors(self):
         return ', '.join([author.name for author in self.author.all()])
 
-    
+
     def save(self, *args, **kwargs):
         if self.review and self.date_review is None:
             self.date_review = now()
