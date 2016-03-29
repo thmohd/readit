f8ab4334933d64d375298e3cb7a7f002f2a1613b
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2016-03-28 21:20:22 -0400

    Register Books in Admin, enhance admin look

--- books/models.py
+++ books/models.py
@@ -4,7 +4,11 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=150)
-    author = models.CharField(max_length=70)
+    author = models.CharField(max_length=70, help_text="Use pen name rather than real name")
     review = models.TextField(blank=True, null=True)
     date_review = models.DateTimeField(blank=True, null=True)
-    is_favourite = models.BooleanField(default=False)
+    is_favourite = models.BooleanField(default=False, verbose_name="Favourite?")
+
+
+    def __str__(self):
+        return self.title
