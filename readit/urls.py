a3e241e1e3fba508b5831a1b2d14d425cf7d285d
Mohammad Althayabeh <althayabeh.mohammad@gmail.com>
2016-03-29 21:26:40 -0400

    add our first url and display logged user

--- readit/urls.py
+++ readit/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from books import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.list_books, name="books")
 ]
